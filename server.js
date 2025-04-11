3ae85fa5f865aad20019901c75b56d1323b11f78
ivandge03 <ivan034124@gmail.com>
2025-04-11 10:50:56 +0300

    Оправена липсваща дефиниция на server

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+const express = require('express'); 
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
     console.log(`Сървърът работи на порт ${PORT}`);
-  });
-  
+});
